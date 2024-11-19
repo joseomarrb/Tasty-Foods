86bc583120ac60c2c250d88e8139a74f8459e791
joseomarrb <omarrosadoj@gmail.com>
2024-11-19 14:13:34 -0400

    Part1 Funcionalidad - Elementos creados con JS

---
+++ javascript/db.js
@@ -0,0 +1,114 @@
+const news = [
+    {
+        title: 'Double Cheeseburger',
+        description: 'Try our double cheeseburger, an explosion of flavor with juicy meat and melted cheese.',
+        image: 'imgs/burger-abob-es.jpg'
+    },
+    {
+        title: 'Delicious Spaghetti Bolognese',
+        description: 'Enjoy our spaghetti with homemade bolognese sauce, prepared with fresh and natural ingredients.',
+        image: 'imgs/pasta-abob-0.jpeg'
+    },
+    {
+        title: 'Pepperoni and Olive Pizza',
+        description: 'Savor our pepperoni and olive pizza, a perfect combination of fresh ingredients and crispy crust.',
+        image: 'imgs/pizza-abob-t.webp'
+    }
+];
+
+const gallery = [
+    {
+        image: 'imgs/galery-asta-pomodoro.jpg',
+        description: 'Imagen de deliciosa Pasta Pomodoro'
+    },
+    {
+        image: 'imgs/galery-hamburguesa.webp',
+        description: 'Imagen de deliciosa Hamburguesa con queso'
+    },
+    {
+        image: 'imgs/galery-albóndigas.webp',
+        description: 'Imagen de deliciosa Pasta con Albondigas'
+    },
+    {
+        image: 'imgs/gallery-pizza2.webp',
+        description: 'Imagen de deliciosa Pizza de Peperoni'
+    },
+    {
+        image: 'imgs/galery-pasta2.jpg',
+        description: 'Imagen de deliciosa Pasta con muchos condimentos naturales'
+    },
+];
+
+const feedbacks = [
+    {   
+        name: 'Carlos M. Rivera',
+        position: 'Food Critic',
+        coment: 'A wonderful place with an excellent selection of burgers. The service was exceptional from the moment I walked in until I finished my meal. I tried the Burger Royal and was blown away by the quality of the ingredients and the perfect combination of flavours.',
+        image: 'imgs/feedback-pasta.png',
+    },
+    {   
+        name: 'María G. López',
+        position: 'Gourmet Enthusiast',
+        coment: 'The food here is amazing. Each dish is prepared with meticulous attention to detail and an obvious passion for cooking. I tried the spaghetti alla siciliana, and I can say that it was a delicious experience, full of authentic and fresh flavors. It is perfect.',
+        image: 'imgs/feedback-pasta.png',
+    }
+];
+
+const products = [
+    {
+        id: 1,
+        name: 'Frenchy Fry',
+        details: 'Get a 20% Discount On First Order',
+        price: 10.90,
+        image: 'imgs/papas.webp'
+    },
+    {
+        id: 2,
+        name: 'Pasta',
+        details: 'Get a 20% Discount On First Order',
+        price: 14.90,
+        image: 'imgs/pasta-removebg-preview.png'
+    },
+    {
+        id: 3,
+        name: 'Sandwich',
+        details: 'Get a 20% Discount On First Order',
+        price: 7.90,
+        image: 'imgs/sandwich.png'
+    },
+    {
+        id: 4,
+        name: 'Spicy Burger',
+        details: 'Get a 20% Discount On First Order',
+        price: 11.90,
+        image: 'imgs/hamburguesa-removebg-preview.png'
+    },
+    {
+        id: 5,
+        name: 'Cold Drinks',
+        details: 'Get a 20% Discount On First Order',
+        price: 8.90,
+        image: 'imgs/food-cola-removebg-preview.png'
+    },
+    {
+        id: 6,
+        name: 'Combo 1',
+        details: 'Get a 20% Discount On First Order',
+        price: 15.90,
+        image: 'imgs/popular-combo1.png'
+    },
+    {
+        id: 7,
+        name: 'Combo 2',
+        details: 'Get a 20% Discount On First Order',
+        price: 14.90,
+        image: 'imgs/popular-combo2.jpg'
+    },
+    {
+        id: 8,
+        name: 'Combo 3',
+        details: 'Get a 20% Discount On First Order',
+        price: 16.90,
+        image: 'imgs/popular-combo3.webp'
+    },
+];
---
+++ javascript/script.js
@@ -0,0 +1,106 @@
+//Variables
+const newsSection = document.querySelector('#news-sc');
+const gallerySection = document.querySelector('#gallery-imgs');
+const feedbackSection = document.querySelector('#feedback-cards');
+const productsSection = document.querySelector('#products');
+const viewProductsSection = document.querySelector('#view-products');
+
+//Eventos
+document.addEventListener("DOMContentLoaded", () => {
+    crearNews(news); 
+    crearGallery(gallery); 
+    crearFeedbacks(feedbacks);
+    crearProducts(products);
+});
+
+//Funciones
+function crearNews(news) {
+    news.forEach( datos => {
+        const {title, description, image} = datos;
+        const newsCard = document.createElement('article');
+        newsCard.classList.add('news__card');
+        newsCard.innerHTML = `
+        <div class='news-c-img'>
+            <img class='news-c-img' src='${image}' alt='Imagen de comida'>
+        </div>
+        <div class='news-text_container'>
+            <p class='news__card--title'>${title}</p>
+            <p class='news__card--paragraph'>${description}</p>
+        </div>
+        `;
+        newsSection.appendChild(newsCard);
+    });
+};
+
+function crearGallery(gallery) {
+    gallery.forEach( datos => {
+        const {description, image} = datos;
+        const imgGallery = document.createElement('img');
+        imgGallery.classList.add('img-gallery');
+        imgGallery.src = `${image}`;
+        imgGallery.alt = `${description}`;
+        gallerySection.appendChild(imgGallery)
+    });
+};
+
+function crearFeedbacks(feedbacks) {
+    feedbacks.forEach( datos => {
+        const {name, coment, position, image} = datos;
+        const feedbackCard = document.createElement('article');
+        feedbackCard.classList.add('feedback-card');
+        feedbackCard.innerHTML = `
+        <div class='feedback-text'>
+            <div class='feedback-text'>
+                <div class="stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                </div>
+            </div>
+            <p class='feedcard--paragraph'>${coment}</p>
+        </div>
+        <div class='feed-card-img'>
+            <div class='feed-person-name'>
+                <p class='person-name'>${name}</p>
+                <p class='person-position'>${position}</p>
+            </div>
+            <img src='${image}' alt="Decoracion con imagen de spaghetti"'>
+        </div>
+        `;
+        feedbackSection.appendChild(feedbackCard);
+    });
+};
+
+function crearProducts(products) {
+    products.forEach( datos => {
+        const {id, name, image, details, price} = datos;
+        const productCard = document.createElement('div');
+        productCard.classList.add('popular-card');
+        productCard.innerHTML = `
+            <div class="button-favorite">
+                <button><i class="fa-solid fa-heart"></i></button>
+            </div>
+        
+            <article class="products--item">
+                <img class="popular__img" src="${image}" alt="">
+                <div class="stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                </div>
+                <p class="popular-name">${name}</p>
+                <p class="popular-details">${details}</p>
+                <p class="popular-price">$${price}</p>
+
+                <div class="popular-buttons">
+                    <button id='${id}' class="btn btn-secundary popular-btn"><i class="fa-solid fa-bag-shopping"></i>Add to Cart</button>
+                </div>
+            </article>
+        `;
+        productsSection.appendChild(productCard);
+    });
+}
